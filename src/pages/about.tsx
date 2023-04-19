4d4c94ccf43eccb1e75ae2f6aaadc12601fe4478
hanna <hannawkozak@gmail.com>
2023-04-19 10:57:37 +0100

    feat: implement about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,44 @@
+import { Carusel } from 'src/components/Carusel';
+
 const about = () => {
-  return <>About</>;
+  return (
+    <div className='container prose mx-auto lg:max-w-5xl p-6 sm:p-12 '>
+      <Carusel />
+      <p>
+        Cześć! Jestem pasjonatem morsowania i chciałbym się z Tobą podzielić
+        moimi doświadczeniami i przemyśleniami na temat tej wspaniałej
+        aktywności na moim blogu.
+      </p>
+      <p>
+        Morsowanie to dla mnie nie tylko sposób na poprawę zdrowia, ale również
+        na wzmocnienie siły woli i pokonanie własnych ograniczeń. Kiedy po raz
+        pierwszy postanowiłem wejść do zimnej wody, było to dla mnie wielkim
+        wyzwaniem. Jednak z czasem nauczyłem się kontrolować swoje ciało i
+        umysł, co pozwoliło mi na regularne kąpiele w zimnej wodzie.
+      </p>
+      <p>
+        Dzięki morsowaniu nauczyłem się, jak ważne jest dbanie o swoje zdrowie i
+        kondycję fizyczną. Kąpiele w zimnej wodzie poprawiają krążenie krwi,
+        wzmacniają układ odpornościowy i pomagają walczyć z chorobami.
+        Dodatkowo, po każdej kąpieli czuję się odprężony i pełen energii, co
+        wpływa pozytywnie na moje samopoczucie i nastrój.
+      </p>
+      <p>
+        Morsowanie to również wspaniała okazja do spędzenia czasu na świeżym
+        powietrzu i kontemplowania piękna natury. Uwielbiam obserwować wschody i
+        zachody słońca nad morzem oraz słuchać szumu fal. Dzięki morsowaniu
+        poznaję również ciekawych ludzi, którzy podobnie jak ja kochają tę
+        aktywność i dzielą się swoimi doświadczeniami.
+      </p>
+      <p>
+        Jestem przekonany, że morsowanie to nie tylko aktywność fizyczna, ale
+        również forma relaksu i odprężenia. Dlatego też zawsze staram się
+        połączyć morsowanie z medytacją i praktykami mindfulness. Dzięki temu
+        potrafię skupić się na teraźniejszości i cieszyć się każdą chwilą
+        spędzoną w zimnej wodzie.
+      </p>
+    </div>
+  );
 };
 
 export default about;
